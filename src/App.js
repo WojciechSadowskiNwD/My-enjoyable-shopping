827e7049479fbe45a037e7169fe17bfbf9861b43
Wojciech Sadowski <wojciechnwd@gmail.com>
2024-10-08 21:23:55 +0200

    Add BurgerNav component, implementation of mobile navigation, styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Logo from './components/Logo';
+import BurgerNav from './components/BurgerNav';
 
 function App() {
 	return (
 		<div className="App">
+      <BurgerNav />
 			<Logo />
 		</div>
 	);
---
+++ src/components/BurgerNav.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import styles from "./BurgerNav.module.scss";
+import { useState } from "react";
+
+function BurgerNav() {
+	const [isOpen, setIsopen] = useState(false);
+
+	function handleClick() {
+		setIsopen(!isOpen);
+	}
+
+	return (
+		<nav className={styles.mobileNav}>
+			<button className={styles.burger} onClick={handleClick}>
+				{isOpen ? (<i class="fas fa-times hide"></i>) : ( <i class="fas fa-bars"></i>)}
+			</button>
+			<Router>
+				<div className={`${styles.links} ${isOpen ? styles.isOpen : ""}`}>
+					<Link to="/">Login</Link>
+					<Link to="/orderPremium">Order Premium</Link>
+					<Link to="/aboutUs">About us</Link>
+					<Link to="/contact">Contact</Link>
+				</div>
+			</Router>
+		</nav>
+	);
+}
+
+export default BurgerNav;
