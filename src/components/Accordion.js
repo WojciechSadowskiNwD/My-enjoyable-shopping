311c92e6f3ff01814b69156e096bac77c4130ed9
Wojciech Sadowski <wojciechnwd@gmail.com>
2024-10-11 22:00:07 +0200

    Refactor Accordion component / Add AccordionItem

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,39 @@
-function Accordion({data}){
-    console.log(data);
-    return (
-        <div>
-            Accordion
-        </div>    
-    )
+import { useState } from "react";
+import styles from "./Accordion.module.scss";
+
+export default function Accordion({ data }) {
+	const [currentOpen, setCurrentOpen] = useState(null);
+
+    const handleToggle = (index) => {
+        setCurrentOpen(currentOpen === index ? null : index);
+    };
+
+
+	return (
+		<div className={styles.accordion_box}>
+			{data.map((item, index) => (
+				<AccordionItem
+					title={item.title}
+                    isOpen={currentOpen === index}
+					key={item.title}
+					text={item.text}
+                    onToggle={()=> handleToggle(index)}
+				/>
+			))}
+		</div>
+	);
 }
 
-export default Accordion;
+function AccordionItem({ title, text, isOpen, onToggle }) {
+	return (
+		<div className={styles.accordion_margin}>
+			<div className={`${styles.accordion} ${styles.accordion_top}`} onClick={onToggle}>
+				<p className={styles.title}>{title}</p>
+				<p className={styles.icon}>+</p>
+			</div>
+			{isOpen && <div className={`${styles.accordion} ${styles.accordion_bottom}`}>
+                <p className={styles.text}>{text}</p>
+            </div>}
+		</div>
+	);
+}
