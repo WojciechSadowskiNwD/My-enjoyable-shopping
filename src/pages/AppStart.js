72637dd4cd675a5d737689d034e5306baa4a717c
Wojciech Sadowski <wojciechnwd@gmail.com>
2024-11-09 00:42:04 +0100

    Created the appearance of key elements for AppStart.js

--- src/pages/AppStart.js
+++ src/pages/AppStart.js
@@ -5,20 +5,39 @@ import Logo from "../components/Logo";
 function AppStart() {
 	return (
 		<div className={styles.app}>
-			<header className={styles.app_header}>
-				<div className={styles.green_circle}></div>
-				<div className={styles.div_size}>
-					<Logo details={styles.logo} imgSize={styles.imgSize} />
+			<div className={styles.bg_shadow}>
+				<header className={styles.app_header}>
+					<div className={styles.green_circle}></div>
+					<div className={styles.div_size}>
+						<Logo details={styles.logo} imgSize={styles.imgSize} />
+					</div>
+					<div className={styles.user_square}>
+						<i class="fa-solid fa-user-large"></i>
+						<p>Welcome David!</p>
+					</div>
+				</header>
+				<div className={styles.app_section_button}>
+					<div className={styles.button_box}>
+						<p>Add a new store and create a shopping list inside</p>
+						<button className={styles.addList_btn}>
+							+ Add new shopping list
+						</button>
+					</div>
 				</div>
-				<div className={styles.user_square}>
-					<i class="fa-solid fa-user-large"></i>
-					<p>Welcome David!</p>
+				<div className={styles.app_main}>
+					<div className={styles.app_title}>
+						<h2>Or choose one of the existing store lists</h2>
+					</div>
+					<div className={styles.section_shop_lists}>
+						<div className={styles.shop_square}>1</div>
+						<div className={styles.shop_square}>2</div>
+						<div className={styles.shop_square}>3</div>
+						<div className={styles.shop_square}>4</div>
+					</div>
 				</div>
-			</header>
-			<div className={styles.app_main}>
-				<div className={styles.section_button}>
-					<p>Add a new store and create a shopping list inside</p>
-					<button className={styles.addList_btn}>+ Add new shopping list</button>
+
+				<div className={styles.logout_box}>
+					<button className={styles.logout_btn}>Logout</button>
 				</div>
 			</div>
 		</div>
