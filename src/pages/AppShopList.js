6cb93fe1532c867fc964dade276d2afed7ccffe9
Wojciech Sadowski <wojciechnwd@gmail.com>
2024-12-20 21:12:11 +0100

    At the very bottom of the AppShopList, the LogoutBtn component is placed just like in AppStart. In addition, a useEffect calling navigate to homepage was placed in the code, on the same principle as AppStart.

--- src/pages/AppShopList.js
+++ src/pages/AppShopList.js
@@ -1,24 +1,36 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useLogin } from "../contexsts/LoginProvider";
 import { useLists } from "../contexsts/ListsProvider";
 import AppCircleHeader from "../components/App_components/AppCircleHeader";
 import AppTopTextBar from "../components/App_components/AppTopTextBar";
 import NewProductForm from "../components/App_components/NewProductForm";
 import AllProducts from "../components/App_components/AllProducts";
-
+import LogoutBtn from "../components/App_components/LogoutBtn";
 
 function AppShopList() {
 	const location = useLocation();
-	const {img, name } = location.state || {};
-	const {state} = useLists();
-	const thisList = state[name]; 
-	
+	const { img, name } = location.state || {};
+	const { state } = useLists();
+	const navigate = useNavigate();
+	const { isAuthenticated } = useLogin();
+	const thisList = state[name];
+
+	// automatic redirect from here to Homepage if false
+	useEffect(() => {
+		if (isAuthenticated === false) {
+			navigate("/", { replace: true });
+		}
+	}, [isAuthenticated, navigate]);
+
 	return (
 		<div className="app">
 			<div className="shadow_bg bg_shadow">
 				<AppCircleHeader />
-                <AppTopTextBar img={img} name={name}/>
-                <NewProductForm thisList={thisList}/>
-				<AllProducts listName={name}/>
+				<AppTopTextBar img={img} name={name} />
+				<NewProductForm thisList={thisList} />
+				<AllProducts listName={name} />
+				<LogoutBtn />
 			</div>
 		</div>
 	);
